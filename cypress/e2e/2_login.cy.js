8293a2979fcb5036ef5d4682fef2e9ba7873838f
Ravishan Lakshitha Premarathne <66440787+ravishan123@users.noreply.github.com>
2023-07-02 15:12:06 +0530

    Update 2_login.cy.js

--- cypress/e2e/2_login.cy.js
+++ cypress/e2e/2_login.cy.js
@@ -1,9 +1,9 @@
 const email = require('../../email.json')
-Login process
-describe('Login', () => {
-  it('should log in successfully', () => {
-    cy.login("waruni+11@smashtaps.com", 'User@1234');
-    // Continue with your assertions or further actions after logging in
-  });
-});
+// Login process
+// describe('Login', () => {
+//   it('should log in successfully', () => {
+//     cy.login("waruni+11@smashtaps.com", 'User@1234');
+//     // Continue with your assertions or further actions after logging in
+//   });
+// });
 
