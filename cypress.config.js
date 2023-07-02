1685eeb11e466d35db65888dded0eafcd5c51e26
Ravishan Lakshitha Premarathne <66440787+ravishan123@users.noreply.github.com>
2023-07-02 07:05:58 +0530

    sda

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-   videoCompression: 15,
+  videoCompression: 15,
+  projectId: "9zgi3f",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
